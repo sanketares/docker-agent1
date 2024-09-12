4a238bad99c2cddc62bb58cc7c88c7a2a469dc84
sanketares <sanketares@gmail.com>
2024-09-11 21:40:59 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_instance" "sanket_inssed" {
     Name = "sanket12345"
   }
 }
+
+resource "aws_instance" "sank" {
+  instance_type = "t2.micro"
+  ami           = "ami-05134c8ef96964280"
+  tags = {
+    Name = "sanke"
+  }
+}
