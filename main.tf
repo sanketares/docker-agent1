d8a6784d27bbb98157f3f71313b3058bada4e0ee
sanketares <sanketares@gmail.com>
2024-09-11 20:33:12 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "sanket_inssed" {
+  instance_type = "t2.micro"
+  ami           = "ami-05134c8ef96964280"
+  tags = {
+    Name = "sanket12345"
+  }
+}
